f15d1f9913b9a526ac49f0c0870e60665b8d50e5
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2019-10-10 09:00:16 +0300

    initial commit

---
+++ src/main/java/com/foo/projecteuler/EulerProblem.java
@@ -0,0 +1,10 @@
+package com.foo.projecteuler;
+
+/**
+ * 
+ * @author Oguz Ozkeroglu 
+ * Created on 2019.10.10
+ */
+public interface EulerProblem {
+	Long getResult();
+}
---
+++ src/main/java/com/foo/projecteuler/Problem10.java
@@ -0,0 +1,41 @@
+package com.foo.projecteuler;
+
+/**
+ * https://projecteuler.net/problem=10
+ * 
+ * <h2>Summation of primes</h2>
+ * 
+ * <p>The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.<br/>
+ * Find the sum of all the primes below two million.</p>
+ * 
+ * @author Oguz Ozkeroglu
+ * Created on 2019.10.10
+ */
+
+public class Problem10 implements EulerProblem {
+
+	public static void main(String[] args) {
+		System.out.println(new Problem10().getResult());
+	}
+
+	@Override
+	public Long getResult() {
+		long sum = 0;
+		for (int i = 2; i < 2_000_000; i++) {
+			if (isPrime(i)) sum += i;
+		}
+
+		return sum;
+	}
+	
+	private boolean isPrime(int number) {
+		if (number <= 1)
+			return false;
+
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if (number % i == 0) return false;
+		}
+
+		return true;
+	}
+}

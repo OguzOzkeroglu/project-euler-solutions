7f08c46d306279efe16a632e916e1a414a413e4d
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2019-10-11 18:25:43 +0300

    Problem 1 is added.

---
+++ src/main/java/com/foo/projecteuler/Problem1.java
@@ -0,0 +1,32 @@
+package com.foo.projecteuler;
+
+/**
+ * <h2>Multiples of 3 and 5</h2>
+ * 
+ * <p>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.</p>
+ * 
+ * @author Oguz Ozkeroglu
+ * Created on 2019.10.11
+ * @see <a href="https://projecteuler.net/problem=1">Multiples of 3 and 5</a>
+ */
+
+public class Problem1 implements EulerProblem {
+	
+	public static void main(String[] args) {
+		System.out.println(new Problem1().getResult());
+	}
+
+	@Override
+	public Long getResult() {
+		long sum = 0;
+
+		for (int i = 3; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+
+		return sum;
+	}
+}

7b419bd2e99ffea6ed1652194134a41c0935a6d4
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2021-01-21 13:40:40 +0300

    add Problem 30

---
+++ src/main/java/com/foo/projecteuler/Problem030.java
@@ -0,0 +1,37 @@
+package com.foo.projecteuler;
+
+import java.util.stream.IntStream;
+
+/**
+ * Digit fifth powers
+ * 
+ * @author Oguz Ozkeroglu
+ * Created on 2021.01.21
+ * @see <a href="https://projecteuler.net/problem=30">Digit fifth powers</a>
+ */
+
+public class Problem030 implements EulerProblem {
+	
+	public static void main(String[] args) {
+		System.out.println(new Problem030().getResult());
+	}
+
+	@Override
+	public Long getResult() {
+		int result = IntStream
+				.range(2, 1_000_000)
+				.filter(number -> number == getSumOfFifthPowers(number))
+				.sum();
+
+		return (long) result;
+	}
+
+	private int getSumOfFifthPowers(int number) {
+		String s = Integer.toString(number);
+
+		return s.chars()
+				.map(c -> Character.getNumericValue(c))
+				.map(digit -> (int) (Math.pow(digit, 5)))
+				.sum();
+	}
+}
